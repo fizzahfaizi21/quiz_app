7f709da67443d4d363330a1bcc4001cc38502c63
Fizzah <fizzahfaizi21@gmail.com>
2025-04-15 22:21:36 -0400

    added the api service

---
+++ lib/services/api_service.dart
@@ -0,0 +1,28 @@
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+import 'lib/models/question.dart';
+
+class ApiService {
+  static Future<List<Question>> fetchQuestions({
+    int amount = 10,
+    int category = 9,
+    String difficulty = 'easy',
+    String type = 'multiple',
+  }) async {
+    final response = await http.get(
+      Uri.parse(
+        'https://opentdb.com/api.php?amount=$amount&category=$category&difficulty=$difficulty&type=$type',
+      ),
+    );
+
+    if (response.statusCode == 200) {
+      final data = json.decode(response.body);
+      List<Question> questions = (data['results'] as List)
+          .map((questionData) => Question.fromJson(questionData))
+          .toList();
+      return questions;
+    } else {
+      throw Exception('Failed to load questions');
+    }
+  }
+}
